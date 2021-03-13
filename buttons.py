626d7bbc172f4ab43177bf1c5328acd24447cf18
JAgrit20 <jagritacharya2020@gmail.com>
2021-03-13 12:21:28 +0530

    button done

--- buttons.py
+++ buttons.py
@@ -7,8 +7,10 @@ def ontap():
 
 b1 = Button(root, text="hello jags",command=ontap)
 b2 = Button(root, text="hello bro!", state = DISABLED)
+b3 = Button(root, text="color dekho",fg="green",bg="red")
 
 b1.pack()
 b2.pack()
+b3.pack()
 
 root.mainloop()
